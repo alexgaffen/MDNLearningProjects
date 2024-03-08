323843cf85784d98f49e44a0b05e12057d7eb19d
Alex Gaffen <84543637+alexgaffen@users.noreply.github.com>
2024-03-08 08:29:55 -0500

    a

--- MDN Playground/index.js
+++ MDN Playground/index.js
@@ -24,3 +24,60 @@ const fruits = ["apples", "bananas", "cherries"];
 for (const fruit of fruits) {
     console.log(fruit);
 }
+
+const fetchPromise1 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+const fetchPromise2 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found");
+const fetchPromise3 = fetch("https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((responses) => {
+        for (const response of responses) {
+            console.log(`${response.url}: ${response.status}`);
+        }
+    })
+    .catch((error) => {
+        console.error(`Failed to fetch: ${error}`);
+    });
+
+async function fetchProducts() {
+    try {
+        const response = await fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+        if (!response.ok) {
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data[0].name);
+    } catch (error) {
+        console.error(`Could not get products: ${error}`);
+    }
+}
+
+fetchProducts();
+
+
+
+function alarm(person, delay) {
+    return new Promise((resolve, reject) => {
+        if (delay < 0) {
+            throw new Error("Alarm delay must not be negative");
+        }
+        setTimeout(() => {
+            resolve(`Wake up, ${person}!`)
+        }, delay);
+    });
+}
+
+button.addEventListener("click", () => {
+    alarm(name.value, delay.value)
+        .then((message) => (output.textContent = message))
+        .catch((error) => (output.textContent = `Couldn't set alarm: ${error}`));
+});
+
+button.addEventListener("click", async () => {
+    try {
+        const message = await alarm(name.value, delay.value);
+        output.textContent = message;
+    } catch (error) {
+        output.textContent = `Couldn't set alarm: ${error}`;
+    }
+});
